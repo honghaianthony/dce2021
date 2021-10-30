a1fdf32269a183ae12ccfe7b651bdc49373bfcfa
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-10-30 13:41:23 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [test, setTest] = useState("");
-  async function getUser() {
-    try {
-      const response = await axios.get('http://localhost:3000/api/users');
-      console.log(response);
-      setTest(response.data.user)
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  getUser();
+  const [test, setTest] = useState([]);
+  useEffect(async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    // console.log(response);
+    setTest(response.data)
+  }, [])
+
   return (
     <div className="App">
-      <p>{test}</p>
-      
+      <p>hello</p>
+      <ul>
+        {test.map(i => <li key={i.id}>{i.title}</li>)}
+      </ul>
     </div>
   );
 }
