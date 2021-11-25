989d2745ac79cbba2c4b0991811c63a07d8422f9
LunarMoon8901 <vuanhnguyet080901@gmail.com>
2021-11-25 11:08:00 +0700

    create Part1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import MainLayout from "../layouts/MainLayout";
+import Part1 from "../components/Part1";
 
 function Home() {
 
     return (
         <>
         <MainLayout>
+
 
             <h1>Home</h1>
         </MainLayout>
