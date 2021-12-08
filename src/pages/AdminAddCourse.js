afe47066a20d437e8175d41f1f0a9e0a0dad3bb4
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-12-08 13:53:03 +0700

    merge fe 08/12

--- src/pages/AdminAddCourse.js
+++ src/pages/AdminAddCourse.js
@@ -1,49 +1,60 @@
-import React from 'react'
+import React from "react";
 import "../assets/styles/AdminAddCourse.css";
-import Navigation from '../components/Navigation';
+import Navigation from "../components/Navigations/Navigation";
 function AdminAddCourse() {
-    return (
-        <div className="AdminAddCourse">
-            <Navigation />
-            <div className="top_decription_link">
-                <h2>Trang chủ - Quản lý khóa học</h2>
+  return (
+    <div className="AdminAddCourse">
+      <Navigation />
+      <div className="top_decription_link">
+        <h2>Trang chủ - Quản lý khóa học</h2>
+      </div>
+      <div className="Container_____AdminAdCourse">
+        <div className="center_AdminAddCourse__container">
+          <div className="center_menu_AdminAddCourse">
+            <p>Hello</p>
+          </div>
+          <div className="center_content_AdminAddCourse">
+            <div className="top_decription_centercontent">
+              <p>Tạo khóa học mới</p>
             </div>
-            <div className="Container_____AdminAdCourse">
-                <div className="center_AdminAddCourse__container">
-                    <div className="center_menu_AdminAddCourse">
-                        <p>Hello</p>
-                    </div>
-                    <div className="center_content_AdminAddCourse">
-                        <div className="top_decription_centercontent">
-                            <p>Tạo khóa học mới</p>
-                        </div>
-                        <div className="middle_decription_centercontent">
-                            <div className="InputName_AddCourse">
-                                <form action="">
-                                    <input type="text" name="id" id="idCourse" placeholder="Nhập tên khóa học mới" />
-                                </form>
-                            </div>
-                            <div className="InputDescription_AddCourse">
-                                <form>
-                                    <textarea name="description" id="decriptionCourse" placeholder="Nhập mô tả"></textarea>
-                                </form>
-                            </div>
-                            <div className="InputTime_AddCourse">
-                            <form>
-                                    <textarea name="time" id="timeCourse" placeholder="Nhập thời gian để hoàn thành khóa học"></textarea>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="btn_Submit_AdminAddCourse">
-                            <button type="submit">
-                                    Tiếp tục
-                            </button>
-                        </div>
-                    </div> 
-                </div>
+            <div className="middle_decription_centercontent">
+              <div className="InputName_AddCourse">
+                <form action="">
+                  <input
+                    type="text"
+                    name="id"
+                    id="idCourse"
+                    placeholder="Nhập tên khóa học mới"
+                  />
+                </form>
+              </div>
+              <div className="InputDescription_AddCourse">
+                <form>
+                  <textarea
+                    name="description"
+                    id="decriptionCourse"
+                    placeholder="Nhập mô tả"
+                  ></textarea>
+                </form>
+              </div>
+              <div className="InputTime_AddCourse">
+                <form>
+                  <textarea
+                    name="time"
+                    id="timeCourse"
+                    placeholder="Nhập thời gian để hoàn thành khóa học"
+                  ></textarea>
+                </form>
+              </div>
             </div>
+            <div className="btn_Submit_AdminAddCourse">
+              <button type="submit">Tiếp tục</button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AdminAddCourse
+export default AdminAddCourse;
