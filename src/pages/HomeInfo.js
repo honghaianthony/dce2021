1cd55387177dfafdf940c1e63e9e236774ec524d
LunarMoon8901 <vuanhnguyet080901@gmail.com>
2021-12-08 11:34:27 +0700

    create info

---
+++ src/pages/HomeInfo.js
@@ -0,0 +1,17 @@
+import MainLayout from "../layouts/MainLayout";
+import Info from "../components/Info";
+
+function HomeInfo() {
+
+    return (
+        <>
+        <MainLayout>
+        git push origin --delete
+            <Info/>
+        </MainLayout>
+        </>
+        // trong pages sẽ chứa layout
+    );
+}
+
+export default HomeInfo;
