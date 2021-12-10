77276de1eb0bd2fe103777ac73f6ee945c081bfb
NgocGiao <hishirokaiyashi@gmail.com>
2021-12-10 16:26:22 +0700

    sua css register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,8 @@ import "../assets/styles/Register.css";
 import { useStore, actions } from "../store";
 import { useHistory } from "react-router-dom";
 import authApi from "../apis/authApi";
-
+// import Hinhlaptrinh2 from "../assets/images/Hinhlaptrinh2.png"
+import hinhlaptrinh2 from "../assets/images/Hinhlaptrinh2.png";
 function Register() {
   const [state, dispatch] = useStore();
   const history = useHistory();
@@ -28,65 +29,111 @@ function Register() {
         history.push("/")
       ) : (
         <div className="Register__container">
+          <div className="img-content-left_register">
+            <img src={hinhlaptrinh2} alt="Hình minh họa" />
+          </div>
           <div className="Register_content">
             <div className="Top_Register_title">
               <span>Tạo Tài Khoản</span>
             </div>
-            <div className="Top_Register_title_line"></div>
+            {/* <div className="Top_Register_title_line"></div> */}
             <div className="center_Register_input">
               <form className="form_register" onSubmit={handleRegister}>
-                <input
-                  type="text"
-                  name="firstname"
-                  id="firstname"
-                  placeholder="họ..."
-                  className="register_input"
-                />
-                <input
-                  type="text"
-                  name="lastname"
-                  id="lastname"
-                  placeholder="tên..."
-                  className="register_input"
-                />
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="example@gmail.com..."
-                  className="register_input"
-                />
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  placeholder="Số điện thoại..."
-                  className="register_input"
-                />
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  placeholder="Tên người dùng..."
-                  className="register_input"
-                  required
-                />
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Mật khẩu..."
-                  className="register_input"
-                  required
-                />
-                <input
-                  type="password"
-                  name="confirmpassword"
-                  id="confirmpassword"
-                  placeholder="Nhập lại Mật khẩu..."
-                  className="register_input"
-                  required
-                />
+                <div className="form_group">
+                  <label className="label-register_container" for="firstname">
+                      Họ
+                  </label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    placeholder="họ..."
+                    className="register_input"
+                  />
+                  <label className="label-register_container" for="lastname">
+                      Tên
+                  </label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    placeholder="tên..."
+                    className="register_input"
+                  />
+                </div>
+                {/* <div className="form_group">
+                  
+                </div> */}
+                <div className="form_group">
+                  <label className="label-register_container" for="email">
+                      Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="example@gmail.com..."
+                    className="register_input"
+                   />
+                </div>
+                <div className="form_group">
+                  <label className="label-register_container" for="phone">
+                      Số điện thoại
+                  </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="Số điện thoại..."
+                    className="register_input"
+                  />
+                </div>
+                <div className="form_group">
+                  <label className="label-register_container" for="Username">
+                      Tên người dùng
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Tên người dùng..."
+                    className="register_input"
+                    required
+                  />
+                </div>
+                <div className="form_group">
+                  <label className="label-register_container" for="password">
+                      Mật khẩu
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Mật khẩu..."
+                    className="register_input"
+                    required
+                  />
+                </div>
+                <div className="form_group">
+                  <label className="label-register_container" for="confirmpassword">
+                      Nhập lại mật khẩu
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmpassword"
+                    id="confirmpassword"
+                    placeholder="Nhập lại Mật khẩu..."
+                    className="register_input"
+                    required
+                  />
+                </div>
+                <div className="form_group">
+                  <label className="label-register_container" for="dateOfBirth">
+                      Ngày sinh
+                  </label>
+                  <input type="date" name="dateOfBirth" id="dateOfBirth" className="register_input"/>
+                </div>
+                
                 <div className="submit__register">
                   <input
                     className="btn-submit-register"
