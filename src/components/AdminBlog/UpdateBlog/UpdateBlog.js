59bcaed2ee5d4594fbd85155fdd2dcf16f27d200
Đặng Ngô Hồng Hải <65769103+honghaianthony@users.noreply.github.com>
2021-12-14 00:21:23 +0700

    Almost done blog

--- src/components/AdminBlog/UpdateBlog/UpdateBlog.js
+++ src/components/AdminBlog/UpdateBlog/UpdateBlog.js
@@ -1,204 +1,127 @@
-import React from "react";
-import AdminPath from "../AdminPath/AdminPath";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./UpdateBlog.css";
-import "react-markdown-editor-lite/lib/index.css";
-import { useState, useEffect } from "react";
-import { CKEditor } from "ckeditor4-react";
+import AdminPath from "../AdminPath/AdminPath";
+import blogsApi from "../../../apis/blogsApi";
+import Search from "../../Search/index";
 import blogApi from "../../../apis/blogsApi";
 import { toast } from "react-toastify";
-import { uploadFile, deleteFile } from "../../../firebase/util";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router-dom";
+function getFormattedDate(date) {
+    return new Date(date).toLocaleDateString();
+}
 
-function UpdateBlog() {
-    // Initialize a markdown parser
-    let isAuth;
-    const [data, setData] = useState(null);
+function BlogListTable() {
+    const [data, setListBlogs] = useState([]);
+    const [filteredData, setFilteredData] = useState(data);
+    const [search, setSearch] = useState("");
+
+    useEffect(async () => {
+        const res = await blogsApi.getAllBlogs();
+        console.log(res);
+        setListBlogs(res);
+    }, []);
+
     const { blogId } = useParams();
-    const [image, setImage] = useState(null);
-    const [url, setUrl] = useState("");
-    const [progress, setProgress] = useState(0);
-    const [inputValues, setInputValues] = useState({
-        title: "",
-        description: "",
-    });
+
+    useEffect(() => {
+        setFilteredData(data);
+    }, [data]);
+
+    useEffect(() => {
+        const newData = data.filter((item) => {
+            return (
+                item.title.toLowerCase().search(search.toLowerCase()) !== -1 ||
+                (item.id + "").indexOf(search) !== -1
+            );
+        });
+        setFilteredData(newData);
+    }, [search]);
+
+    const [dataDelete, setDataDelete] = useState();
 
     useEffect(async () => {
         const res = await blogApi.getBlogById(blogId);
-        setData(res);
+        setDataDelete(res);
     }, [blogId]);
 
-    // update
-
-    // const [title, setBlogTitle] = useState();
-    // const [description, setBlogDes] = useState();
-
-    // useEffect(async () => {
-    //     const res = await blogApi.getBlogById(blogId);
-    //     setBlogTitle(res.title);
-    // }, [blogId]);
-
-    // useEffect(async () => {
-    //     const res = await blogApi.getBlogById(blogId);
-    //     setBlogDes(res.description);
-    // }, [blogId]);
-
-    useEffect(() => {
-        if (image !== null) {
-        uploadFile(
-            image,
-            (progress) => {
-            setProgress(progress);
-            },
-            (url) => {
-            setUrl(url);
+    const handleDelete = async (blog) => {
+        try {
+            let response = await blogApi.deleteBlog(blog.id);
+            if (response && response.errCode === 0) {
+                await blogApi.getAllBlogs();
+                toast.success("Xóa thành công");
+            } else {
+                toast.success("Xóa thất bại");
             }
-        );
-        }
-    }, [image]);
-
-    // Finish!
-    function handleEditorChange({ editor }) {
-        setData(editor.getData());
-    }
-
-    const handleOnChangeInput = (event) => {
-        const { name, value } = event.target;
-        setInputValues({ ...inputValues, [name]: value });
-    };
-
-    const imageChange = (e) => {
-        if (e.target.files[0]) {
-        setImage(e.target.files[0]);
+        } catch (error) {
+            console.log(error);
         }
     };
 
-    const deleteImage = () => {
-        deleteFile(
-        url,
-        () => {
-            toast.success("Xóa thành công");
-            setProgress(0);
-            setUrl("");
-            document.getElementById("add-blog-cover-image").value = null;
-        },
-        (error) => {
-            toast.error("Lỗi!!!");
-        }
-        );
-    };
-
-    const sendBlog = async () => {
-        const dataBlog = {
-          userId: 1,
-          title: inputValues.title,
-          content: data,
-          description: inputValues.description,
-          coverImage: url,
-        };
-        const res = await blogApi.postBlog(dataBlog);
-        if (res) {
-          toast.success("Đăng bài blog thành công");
+    const listBlogs = () => {
+        if (filteredData.length > 0) {
+            return filteredData.map((item, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.User.userName}</td>
+                        <td>{getFormattedDate(item.createdAt)}</td>
+                        <td>{getFormattedDate(item.updatedAt)}</td>
+                        <td>
+                            <Link to={`/adminUpdateBlog/${item.id}`}>
+                                <button className="view-btn">Chỉnh sửa</button>
+                            </Link>
+                        </td>
+                        <td>
+                            <Link to={`/adminUpdateBlog`}>
+                                <button
+                                    className="view-btn-delete"
+                                    onClick={() => handleDelete(item)}
+                                >
+                                    Xóa
+                                </button>
+                            </Link>
+                        </td>
+                    </tr>
+                );
+            });
         } else {
-          toast.error("Đăng bài blog thất bại");
+            return <h2>Loading..</h2>;
         }
     };
-
-    // const handleChangeBlog = async (e) => {
-    //     e.preventDefault();
-    //     const dataBlog = {
-    //     userId: 1,
-    //     title: title,
-    //     content: data,
-    //     description: description,
-    //     coverImage: url,
-    //     };
-    //     const res = await blogApi.updateBlogById(dataBlog);
-    //     if (res) {
-    //     toast.success("Cập nhật bài blog thành công");
-    //     } else {
-    //     toast.error("Cập nhật bài blog thất bại");
-    //     }
-    // };
-
     return (
-        <>
-            <div className="update-adminblog-container">
+        <div className="bloglisttable-container">
+            <div className="bloglist">
                 <AdminPath />
-                <div className="update-blog-main">
-                    <div className="update-blog-sidebar"></div>
-                    <div className="update-blog">
-                    <h1>Chỉnh sửa bài blog</h1>
-                    <div className="update-blog-title">
-                        <input
-                        type="text"
-                        name="title"
-                        id="update-blog-title"
-                        //placeholder="Nhập tiêu đề bài viết"
-                        onChange={handleOnChangeInput}
-                        // value={title}
-                        // onChange={(event) => setBlogTitle(event.target.value)}
-                        />
-                    </div>
-                    <div className="update-blog-description">
-                        <input
-                        type="text"
-                        name="description"
-                        id="update-blog-description"
-                        placeholder="Nhập mô tả cho bài viết"
-                        onChange={handleOnChangeInput}
-                        // value={description}
-                        // onChange={(event) => setBlogDes(event.target.value)}
-                        />
-                    </div>
-                    <div className="update-blog-cover-image">
-                        {progress < 100 && (
-                        <input
-                            type="file"
-                            accept="image/*"
-                            name="coverImage"
-                            id="update-blog-cover-image"
-                            placeholder="Link hình ảnh"
-                            onChange={imageChange}
-                        />
-                        )}
-                        {progress > 0 && (
-                        <div className="upload-image">
-                            <progress
-                            className="progress-bar"
-                            value={progress}
-                            max="100"
-                            />
-                            <br />
-                            <i class="far fa-times-circle" onClick={deleteImage}></i>
-                            <img
-                            className="preview-image"
-                            src={url || "http://via.placeholder.com/300"}
-                            alt="firebase-image"
-                            />
-                        </div>
-                        )}
-                    </div>
-                    <div className="update-blog-cover"></div>
-                    <div className="update-blog-content">
-                        <CKEditor
-                        config={{
-                            extraPlugins: `justify`,
-                        }}
-                        onChange={(editor) => {
-                            handleEditorChange(editor);
-                        }}
-                        />
-                    </div>
-                    <div className="update-blog-button">
-                        <button type="button" className="update-blog-btn" onClick={sendBlog}>
-                        Cập nhật
-                        </button>
-                    </div>
-                    </div>
+                <div className="bloglist-container">
+                    <h2 className="bloglist-title"> Danh sách blog </h2>
+                    <Search
+                        placeholder="Nhập tên blog cần tìm"
+                        className="bloglist-search"
+                        value={search}
+                        onChange={setSearch}
+                    />
                 </div>
             </div>
-        </>
+            <h2 className="bloglist-tablename">Thông tin blog</h2>
+            <table className="bloglisttable">
+                <thead className="bloglisttable-header">
+                    <tr className="bloglisttable-row">
+                        <th>ID</th>
+                        <th>Tên blog</th>
+                        <th>Admin</th>
+                        <th>Ngày tạo</th>
+                        <th>Update</th>
+                        <th>Chỉnh sửa</th>
+                        <th>Xóa</th>
+                    </tr>
+                </thead>
+                <tbody className="bloglisttable-body">{listBlogs()}</tbody>
+            </table>
+        </div>
     );
 }
 
-export default UpdateBlog;
+export default BlogListTable;
