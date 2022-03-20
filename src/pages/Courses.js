62ab79667c77c4e5d105ee6c8ad205279ea94e1b
Duy Nguyễn <nguyenngockhuongduy172@gmail.com>
2022-03-20 15:16:24 +0700

    test helmet

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,15 +1,21 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
 import CoursesMain from '../components/Courses/index';
+import { Helmet } from "react-helmet-async";
 
 function Courses () {
     return (
-        <>
-            <MainLayout>
-                <CoursesMain />
-            </MainLayout>
-        </>
-    )
+      <>
+        <Helmet prioritizeSeoTags>
+          <title>DCE 2021 - Course</title>
+          <link rel="course" href="https://dce2021.ml/courses" />
+          <meta name="Course" value="DCECourse" />
+        </Helmet>
+        <MainLayout>
+          <CoursesMain />
+        </MainLayout>
+      </>
+    );
 }
 
 export default Courses;
