55a6c65a5629c7716cd840a0e56f6ad18d954a25
Anthony Hai <namsaoquan@gmail.com>
2022-03-24 08:36:29 +0700

    Fix slug

--- src/components/Blogs/BlogDetail/BlogDetail.js
+++ src/components/Blogs/BlogDetail/BlogDetail.js
@@ -9,190 +9,209 @@ import { useStore } from "../../../store";
 import { io } from "socket.io-client";
 
 function BlogDetail() {
-  const [data, setData] = useState(null);
-  const { blogId } = useParams();
-  const [state, dispatch] = useStore();
-  const [comment, setComment] = useState([]);
-  const [commentInput, setCommentInput] = useState("");
-  const [showMoreCmt, setShowMoreCmt] = useState(5);
+    const [data, setData] = useState(null);
+    const { id } = useParams();
+    const [state, dispatch] = useStore();
+    const [comment, setComment] = useState([]);
+    const [commentInput, setCommentInput] = useState("");
+    const [showMoreCmt, setShowMoreCmt] = useState(5);
 
-  useEffect(async () => {
-    const res = await blogsApi.getBlogById(blogId);
-    setData(res);
-    const cmt = await blogsApi.getAllBlogCommentByBlogId(blogId);
-    setComment(cmt);
-  }, []);
+    useEffect(async () => {
+        const res = await blogsApi.getBlogById(id);
+        setData(res);
+        const cmt = await blogsApi.getAllBlogCommentByBlogId(id);
+        setComment(cmt);
+    }, []);
 
-  //socket.io
-  const socket = useRef();
-  // const socket = io("http://localhost:3000");
-  useEffect(() => {
-    socket.current = io("http://localhost:3000");
-    socket.current.emit("join-room", blogId);
-    socket.current.on("receive-comment", (data) => {
-      setComment([...comment, data]);
-    });
-  }, [blogId, socket.current]);
+    //socket.io
+    const socket = useRef();
+    // const socket = io("http://localhost:3000");
+    useEffect(() => {
+        socket.current = io("https://dce-uit.herokuapp.com");
+        socket.current.emit("join-room", id);
+        socket.current.on("receive-comment", (data) => {
+            setComment([...comment, data]);
+        });
+    }, [id, socket.current]);
 
-  const renderComment = () => {
-    return comment
-      .map((item, index) => {
-        return (
-          index > comment.length - showMoreCmt && (
-            <div className="blog-comment-other" key={item.id}>
-              <div className="blog-comment-user">
-                <div className="blog-comment-userinfo">
-                  <div className="blog-comment-avatar">
-                    <IoPersonCircleSharp />
-                  </div>
-                  <div className="blog-comment-name">
-                    <p>{`${item.User.lastName} ${item.User.firstName}`}</p>
-                  </div>
-                  <div className="blog-comment-date">
-                    <p>{new Date(item.updatedAt).toLocaleDateString()}</p>
-                  </div>
-                </div>
-                <div className="blog-comment-content">
-                  <p>{item.content}</p>
-                </div>
-              </div>
-            </div>
-          )
-        );
-      })
-      .reverse();
-  };
-  const sendComment = (e) => {
-    e.preventDefault();
-    const comment = commentInput;
-    socket.current.emit("send-comment", comment, state.userId, blogId);
-    setCommentInput("");
-  };
-  const handleCommentChange = (e) => {
-    setCommentInput(e.target.value);
-  };
-  return (
-    <div className="blog-container">
-      <BlogPath />
-      {data ? (
-        <div className="blog-detail">
-          <div className="blog-detail-main">
-            <div className="blog-main">
-              <div className="blog-author">
-                <span>Tác giả:</span>
-                <span className="blog-author-name">{`${data.User.lastName} ${data.User.firstName}`}</span>
-                <i className="blog-time-icon">
-                  <BsCircle />
-                </i>
-                <span className="blog-time">
-                  {new Date(data.updatedAt).toLocaleDateString()}
-                </span>
-              </div>
-              <div className="blog-main-content">
-                <div className="blog-cover-img">
-                  <img src={data.coverImage} alt="blog-cover-img" />
-                </div>
-                <div
-                  className="blog-content"
-                  dangerouslySetInnerHTML={{
-                    __html: data.content,
-                  }}
-                ></div>
-              </div>
-            </div>
-            <div className="blog-comment">
-              {state.isAuthenticated && (
-                <>
-                  <div className="blog-comment-rating">
-                    <i className="blog-item-rating">
-                      <BsFillStarFill />
-                    </i>
-                    <i className="blog-item-rating">
-                      <BsFillStarFill />
-                    </i>
-                    <i className="blog-item-rating">
-                      <BsFillStarFill />
-                    </i>
-                    <i className="blog-item-rating">
-                      <BsFillStarFill />
-                    </i>
-                    <i className="blog-item-rating">
-                      <BsFillStarFill />
-                    </i>
-                    <p>
-                      5.0 <i>(4 đánh giá)</i>
-                    </p>
-                  </div>
-                  <p className="blog-comment-number">
-                    {comment.length} bình luận
-                  </p>
-                  <form onSubmit={sendComment}>
-                    <div className="blog-comment-write">
-                      <div className="blog-comment-avatar">
-                        <IoPersonCircleSharp />
-                      </div>
-                      <div className="blog-comment-field">
-                        <textarea
-                          id="blog-cmt-field"
-                          name="comment"
-                          placeholder="Viết bình luận của bạn"
-                          rows={3}
-                          value={commentInput}
-                          onChange={handleCommentChange}
-                        ></textarea>
-                        <div className="blog-comment-button">
-                          <button type="submit" className="blog-cmt-btn">
-                            Đăng
-                          </button>
+    const renderComment = () => {
+        return comment
+            .map((item, index) => {
+                return (
+                    index > comment.length - showMoreCmt && (
+                        <div className="blog-comment-other" key={item.id}>
+                            <div className="blog-comment-user">
+                                <div className="blog-comment-userinfo">
+                                    <div className="blog-comment-avatar">
+                                        <IoPersonCircleSharp />
+                                    </div>
+                                    <div className="blog-comment-name">
+                                        <p>{`${item.User.lastName} ${item.User.firstName}`}</p>
+                                    </div>
+                                    <div className="blog-comment-date">
+                                        <p>
+                                            {new Date(
+                                                item.updatedAt
+                                            ).toLocaleDateString()}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="blog-comment-content">
+                                    <p>{item.content}</p>
+                                </div>
+                            </div>
                         </div>
-                      </div>
+                    )
+                );
+            })
+            .reverse();
+    };
+    const sendComment = (e) => {
+        e.preventDefault();
+        const comment = commentInput;
+        socket.current.emit("send-comment", comment, state.userId, id);
+        setCommentInput("");
+    };
+    const handleCommentChange = (e) => {
+        setCommentInput(e.target.value);
+    };
+    return (
+        <div className="blog-container">
+            <BlogPath />
+            {data ? (
+                <div className="blog-detail">
+                    <div className="blog-detail-main">
+                        <div className="blog-main">
+                            <div className="blog-author">
+                                <span>Tác giả:</span>
+                                <span className="blog-author-name">{`${data.User.lastName} ${data.User.firstName}`}</span>
+                                <i className="blog-time-icon">
+                                    <BsCircle />
+                                </i>
+                                <span className="blog-time">
+                                    {new Date(
+                                        data.updatedAt
+                                    ).toLocaleDateString()}
+                                </span>
+                            </div>
+                            <div className="blog-main-content">
+                                <div className="blog-cover-img">
+                                    <img
+                                        src={data.coverImage}
+                                        alt="blog-cover-img"
+                                    />
+                                </div>
+                                <div
+                                    className="blog-content"
+                                    dangerouslySetInnerHTML={{
+                                        __html: data.content,
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
+                        <div className="blog-comment">
+                            {state.isAuthenticated && (
+                                <>
+                                    <div className="blog-comment-rating">
+                                        <i className="blog-item-rating">
+                                            <BsFillStarFill />
+                                        </i>
+                                        <i className="blog-item-rating">
+                                            <BsFillStarFill />
+                                        </i>
+                                        <i className="blog-item-rating">
+                                            <BsFillStarFill />
+                                        </i>
+                                        <i className="blog-item-rating">
+                                            <BsFillStarFill />
+                                        </i>
+                                        <i className="blog-item-rating">
+                                            <BsFillStarFill />
+                                        </i>
+                                        <p>
+                                            5.0 <i>(4 đánh giá)</i>
+                                        </p>
+                                    </div>
+                                    <p className="blog-comment-number">
+                                        {comment.length} bình luận
+                                    </p>
+                                    <form onSubmit={sendComment}>
+                                        <div className="blog-comment-write">
+                                            <div className="blog-comment-avatar">
+                                                <IoPersonCircleSharp />
+                                            </div>
+                                            <div className="blog-comment-field">
+                                                <textarea
+                                                    id="blog-cmt-field"
+                                                    name="comment"
+                                                    placeholder="Viết bình luận của bạn"
+                                                    rows={3}
+                                                    value={commentInput}
+                                                    onChange={
+                                                        handleCommentChange
+                                                    }
+                                                ></textarea>
+                                                <div className="blog-comment-button">
+                                                    <button
+                                                        type="submit"
+                                                        className="blog-cmt-btn"
+                                                    >
+                                                        Đăng
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </>
+                            )}
+                            <div className="blog-comment-others">
+                                {comment.length > 0 && renderComment()}
+                            </div>
+                            {showMoreCmt < comment.length && (
+                                <div className="blog-comment-show-more">
+                                    <button
+                                        onClick={() => {
+                                            setShowMoreCmt(showMoreCmt + 5);
+                                        }}
+                                    >
+                                        Xem thêm bình luận{" "}
+                                        <i
+                                            class="fa fa-angle-down"
+                                            aria-hidden="true"
+                                        ></i>
+                                    </button>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                  </form>
-                </>
-              )}
-              <div className="blog-comment-others">
-                {comment.length > 0 && renderComment()}
-              </div>
-              {showMoreCmt < comment.length && (
-                <div className="blog-comment-show-more">
-                  <button
-                    onClick={() => {
-                      setShowMoreCmt(showMoreCmt + 5);
-                    }}
-                  >
-                    Xem thêm bình luận{" "}
-                    <i class="fa fa-angle-down" aria-hidden="true"></i>
-                  </button>
+                    <div className="blog-sidebar">
+                        <div className="blog-author-info">
+                            <h3>TÁC GIẢ</h3>
+                            <div className="author">
+                                <div className="author-avatar">
+                                    <IoPersonCircleSharp />
+                                </div>
+                                <div className="author-name">
+                                    <p>{`${data.User.lastName} ${data.User.firstName}`}</p>
+                                </div>
+                                <div className="author-info">
+                                    <p>Email: {data.User.email}</p>
+                                    <p>
+                                        Thành viên từ{" "}
+                                        {new Date(
+                                            data.User.createdAt
+                                        ).toLocaleDateString()}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              )}
-            </div>
-          </div>
-          <div className="blog-sidebar">
-            <div className="blog-author-info">
-              <h3>TÁC GIẢ</h3>
-              <div className="author">
-                <div className="author-avatar">
-                  <IoPersonCircleSharp />
-                </div>
-                <div className="author-name">
-                  <p>{`${data.User.lastName} ${data.User.firstName}`}</p>
-                </div>
-                <div className="author-info">
-                  <p>Email: {data.User.email}</p>
-                  <p>
-                    Thành viên từ{" "}
-                    {new Date(data.User.createdAt).toLocaleDateString()}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+            ) : (
+                <div className="loader"></div>
+            )}
         </div>
-      ) : (
-        <div className="loader"></div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default BlogDetail;
--- src/components/Blogs/BlogItem.js
+++ src/components/Blogs/BlogItem.js
@@ -1,49 +1,108 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { GrView } from 'react-icons/gr';
-import { BsShare, BsFillStarFill, BsCircle } from 'react-icons/bs';
+import React from "react";
+import { Link } from "react-router-dom";
+import { GrView } from "react-icons/gr";
+import { BsShare, BsFillStarFill, BsCircle } from "react-icons/bs";
 
 function BlogItem(props) {
-  return (
-    <>
-      <div className='blog-item'>
-        <Link className='blog-item-link' to={props.path}>
-            <figure className='blog-item-wrap' data-category={props.label}>
-              <img
-              className='blog-item-img'
-              alt={props.title}
-              src={props.src}
-              />
-            </figure>
-            <div className='blog-item-content'>
-              <div className='blog-item-info' data-category={props.info}>
-                <h5 className='blog-item-title'>{props.title}</h5>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <p className='blog-item-description'>{props.description}</p>
-             </div>
-              <div className='blog-item-author'>
-                <div className='blog-text-left'>
-                    <span>Tác giả:</span>
-                    <span className='blog-author-name'>{props.author}</span>
-                    <i className='blog-time-icon'><BsCircle/></i>
-                    <span className='blog-time'>{props.time}</span>
-                </div>
-                {/* <div className='blog-text-right'>
+    function ChangeToSlug(str) {
+        // Chuyển hết sang chữ thường
+        str = str.toLowerCase();
+
+        // xóa dấu
+        str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, "a");
+        str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, "e");
+        str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, "i");
+        str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, "o");
+        str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, "u");
+        str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, "y");
+        str = str.replace(/(đ)/g, "d");
+
+        // Xóa ký tự đặc biệt
+        str = str.replace(/([^0-9a-z-\s])/g, "");
+
+        // Xóa khoảng trắng thay bằng ký tự -
+        str = str.replace(/(\s+)/g, "-");
+
+        // xóa phần dự - ở đầu
+        str = str.replace(/^-+/g, "");
+
+        // xóa phần dư - ở cuối
+        str = str.replace(/-+$/g, "");
+
+        // return
+        return str;
+    }
+    return (
+        <>
+            <div className="blog-item">
+                <Link
+                    className="blog-item-link"
+                    to={
+                        "/blog/" +
+                        ChangeToSlug(props.title) +
+                        "." +
+                        props.blogId +
+                        ".html"
+                    }
+                >
+                    <figure
+                        className="blog-item-wrap"
+                        data-category={props.label}
+                    >
+                        <img
+                            className="blog-item-img"
+                            alt={props.title}
+                            src={props.src}
+                        />
+                    </figure>
+                    <div className="blog-item-content">
+                        <div
+                            className="blog-item-info"
+                            data-category={props.info}
+                        >
+                            <h5 className="blog-item-title">{props.title}</h5>
+                            <i className="blog-item-rating">
+                                <BsFillStarFill />
+                            </i>
+                            <i className="blog-item-rating">
+                                <BsFillStarFill />
+                            </i>
+                            <i className="blog-item-rating">
+                                <BsFillStarFill />
+                            </i>
+                            <i className="blog-item-rating">
+                                <BsFillStarFill />
+                            </i>
+                            <i className="blog-item-rating">
+                                <BsFillStarFill />
+                            </i>
+                            <p className="blog-item-description">
+                                {props.description}
+                            </p>
+                        </div>
+                        <div className="blog-item-author">
+                            <div className="blog-text-left">
+                                <span>Tác giả:</span>
+                                <span className="blog-author-name">
+                                    {props.author}
+                                </span>
+                                <i className="blog-time-icon">
+                                    <BsCircle />
+                                </i>
+                                <span className="blog-time">{props.time}</span>
+                            </div>
+                            {/* <div className='blog-text-right'>
                     <i className='blog-icon-view'><GrView/></i>
                     <span className='blog-view'>{props.view}</span>
                     <i className='blog-icon-share'><BsShare/></i>
                     <span className='blog-share'>0</span>
                 </div> */}
-              </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
-        </Link>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default BlogItem;
