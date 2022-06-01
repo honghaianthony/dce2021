8612c18c2305b3e4f5b07fe5d2ae0a0528e28766
Duy Nguyễn <nguyenngockhuongduy172@gmail.com>
2022-06-01 22:15:49 +0700

    hot fix blog

--- src/components/Blogs/BlogItem.js
+++ src/components/Blogs/BlogItem.js
@@ -6,40 +6,55 @@ import { BsShare, BsFillStarFill, BsCircle } from 'react-icons/bs';
 function BlogItem(props) {
   return (
     <>
-      <div className='blog-item'>
-        <Link className='blog-item-link' to={props.path}>
-            <figure className='blog-item-wrap' data-category={props.label}>
-              <img
-              className='blog-item-img'
-              alt={props.title}
-              src={props.src}
-              />
-            </figure>
-            <div className='blog-item-content'>
-              <div className='blog-item-info' data-category={props.info}>
-                <h5 className='blog-item-title'>{props.title}</h5>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <i className='blog-item-rating'><BsFillStarFill/></i>
-                <p className='blog-item-description'>{props.description}</p>
-             </div>
-              <div className='blog-item-author'>
-                <div className='blog-text-left'>
-                    <span>Tác giả:</span>
-                    <span className='blog-author-name'>{props.author}</span>
-                    <i className='blog-time-icon'><BsCircle/></i>
-                    <span className='blog-time'>{props.time}</span>
-                </div>
-                {/* <div className='blog-text-right'>
+      <div className="blog-item">
+        <Link className="blog-item-link" to={props.path}>
+          <figure className="blog-item-wrap" data-category={props.label}>
+            <img className="blog-item-img" alt={props.title} src={props.src} />
+          </figure>
+          <div className="blog-item-content">
+            <div className="blog-item-info" data-category={props.info}>
+              <h5 className="blog-item-title">{props.title}</h5>
+              <i className="blog-item-rating">
+                <BsFillStarFill />
+              </i>
+              <i className="blog-item-rating">
+                <BsFillStarFill />
+              </i>
+              <i className="blog-item-rating">
+                <BsFillStarFill />
+              </i>
+              <i className="blog-item-rating">
+                <BsFillStarFill />
+              </i>
+              <i className="blog-item-rating">
+                <BsFillStarFill />
+              </i>
+              <div
+                className="blog-item-description"
+                dangerouslySetInnerHTML={{
+                  __html: props.content.slice(0, 500) + '...</p>',
+                }}
+              >
+                {/* {props.content.slice(0, 100) + '...</p>'} */}
+              </div>
+            </div>
+            <div className="blog-item-author">
+              {/* <div className="blog-text-left">
+                <span>Tác giả:</span>
+                <span className="blog-author-name">{props.author}</span>
+                <i className="blog-time-icon">
+                  <BsCircle />
+                </i>
+                <span className="blog-time">{props.time}</span>
+              </div> */}
+              {/* <div className='blog-text-right'>
                     <i className='blog-icon-view'><GrView/></i>
                     <span className='blog-view'>{props.view}</span>
                     <i className='blog-icon-share'><BsShare/></i>
                     <span className='blog-share'>0</span>
                 </div> */}
-              </div>
             </div>
+          </div>
         </Link>
       </div>
     </>
